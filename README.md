bd7bcc4a7aa0496e10872b5cc285dc5d1f4d0d1a
Shavii <140500938+Sh4vii@users.noreply.github.com>
2023-11-17 10:57:03 +0100

    Add files via upload
    ReadMe, delete button commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Beginner Web-development Project - Todo
+
+This is a beginner ToDo list project.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+  This project focuses on creating a Todo list web application. It involves implementing features to persistently store list items on the device's local storage, ensuring they remain intact even after the page is refreshed.
+
+### The challenge
+Users should be able to:
+
+- Add items to the list
+- Tick items completed from the list
+- Delete individual list item
+- Clear the whole list
+
+### Screenshot
+
+![Preview](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [View Project](https://sh4vii.github.io/ToDo/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktop-first workflow
+- Javascrip
+
+### What I learned
+
+- How to store data in local storage
+
+## Author
+
+- Name: Shavii
+
+
--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const clearList = document.querySelector(".clear-list");
 
+
 //This gets the array of items from local storage or sets items to an empty array 
-const items = JSON.parse(localStorage.getItem("items")) || [];
-console.log(items);
+let items = JSON.parse(localStorage.getItem("items")) || [];
 
 function addItem(e){
-    console.log(e);
     e.preventDefault(); //stops the page from reloading
     const text = (this.querySelector("[name=item]").value);
 
+    //creating a time stamp for list items
     const create = new Date();
 
     const date = create.getDate();
@@ -29,9 +29,8 @@ function addItem(e){
 
     const dateCreated = `${date < 10 ? '0' : ''}${date}/${month < 10 ? '0' : ''}${month}/${year < 10 ? '0' : ''}${year}`;
     const timeCreated = `${hour < 10 ? '0' : ''}${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
- 
-    console.log({theDay, dateCreated, timeCreated});
 
+    //item object to with its attributes
     const  item = {
         text : text,
         dayCreated : theDay,
@@ -40,6 +39,8 @@ function addItem(e){
         done : false
     }
 
+
+    //add the item to the items array and also to the local storage
     items.push(item);
     populateList(items, itemsList);
     localStorage.setItem("items", JSON.stringify(items));
@@ -50,11 +51,26 @@ function populateList(plates=[], platesList){
     platesList.innerHTML = plates.map((plate, i)=>{
         return `<li>
         <p>${plate.timeCreated} - ${plate.dayCreated}, ${plate.dateCreated}</p>
+            <div class="list-item">
             <input class="list" type="checkbox" data-index=${i} id="item${i}" ${plate.done ? "checked" : ""}/>
-            <label for="item${i}">${plate.text}</lable>
-            
+            <p for="item${i}">${plate.text}</p>
+            <button class="delete">Delete</button>
+            </div>
         </li>`;
     }).join("");
+
+    const itemDelete = document.querySelectorAll(".delete");
+    
+    for (let i = 0; i < itemDelete.length; i++) {
+        itemDelete[i].addEventListener("click", ()=>{
+            plates.splice(i, 1);
+            console.log(plates);
+            items = plates;
+            localStorage.setItem("items", JSON.stringify(items));
+            populateList(items, itemsList);
+        })
+    }
+    
 }
 
 function toggleDone(e){
@@ -75,3 +91,5 @@ addItems.addEventListener("submit", addItem);
 itemsList.addEventListener("click", toggleDone);
 
 populateList(items, itemsList);
+
+
